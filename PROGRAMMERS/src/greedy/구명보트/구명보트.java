bdc0483b3e48f696e49cc3230a14908f63bc34cc
HYEONYY <hiii456@hanmail.net>
2022-02-06 22:36:01 +0900

    code : PROGRAMMERS Lv2 구명보트

---
+++ PROGRAMMERS/src/greedy/구명보트/구명보트.java
@@ -0,0 +1,19 @@
+package greedy.구명보트;
+
+import java.util.*;
+
+public class 구명보트 {
+	public int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+        int min = 0;
+        for(int i = people.length-1; i>=min; i--){
+            if(people[min] + people[i] <= limit){
+                min++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
